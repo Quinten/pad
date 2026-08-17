434a52dab9283f39845010ab94ca47dc923f4196
Quinten Clause <quintenclause@hotmail.com>
2026-08-17 17:27:24 +0200

    Fix iphone pinch/zoom/pan

--- src/editor/interaction.js
+++ src/editor/interaction.js
@@ -285,7 +285,7 @@ export function initInteraction(getAutoSave) {
             }
         });
 
-        if (window.PointerEvent && (navigator.maxTouchPoints === undefined || navigator.maxTouchPoints <= 1)) {
+        if (window.PointerEvent && !('ontouchstart' in window) && (navigator.maxTouchPoints === undefined || navigator.maxTouchPoints === 0)) {
             container.addEventListener('pointerdown', e => {
                 try { e.preventDefault(); } catch (err) { /* ignore */ }
                 if (spaceDown) {
